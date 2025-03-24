69af7d76314ba88aa77f4320c6d5c3dc9322f870
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-03-24 09:33:26 +0545

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Waterflow Technology on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
