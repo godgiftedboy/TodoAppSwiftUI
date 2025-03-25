ab084bbee2a3419d046fb53ee2e6832c127a07c0
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-03-25 17:04:12 +0545

    chore: profile logic added - fetch user details and logout

--- todoapp/Views/ ProfileView.swift
+++ todoapp/Views/ ProfileView.swift
@@ -13,44 +13,16 @@ struct _ProfileView: View {
         //Trailing Closure syntax
         NavigationView{
             VStack{
-                //Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.blue)
-                    .frame(width: 125,height: 125)
-                    .padding()
-            
-                //Info: Name, Email
-                VStack(alignment: .leading, content: {
-                    HStack{
-                        Text("Name")
-                            .bold()
-                        Text("Usernmae")
-                    }.padding()
-                    HStack{
-                        Text("Email")
-                            .bold()
-                        Text("Usernmae")
-                    }.padding()
-                    HStack{
-                        Text("Member Since")
-                            .bold()
-                        Text("Usernmae")
-                    }.padding()
+                if let user = viewModel.user {
+                    profileView(user: user)
+                }else{
+                    Text("Loading profile.....")
                 }
-                )
-                
-                //Sign out
-                CustomButton(title: "Logout", background: .blue, action: {
-                    //logout action
-                })
-                .frame(height: 80)
-                .padding()
-                
-                Spacer()
                 
             }.navigationTitle("Profile")
+        }
+        .onAppear{
+            viewModel.fetchUser()
         }
         //equivalent to:
         //        NavigationView(content: {
@@ -58,6 +30,47 @@ struct _ProfileView: View {
         //                // Your views here
         //            }.navigationTitle("Profile")
         //        })
+    }
+    @ViewBuilder
+    func profileView(user: User) -> some View{
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(.blue)
+            .frame(width: 125,height: 125)
+            .padding()
+    
+        //Info: Name, Email
+        VStack(alignment: .leading, content: {
+            HStack{
+                Text("Name")
+                    .bold()
+                Text(user.name)
+            }.padding()
+            HStack{
+                Text("Email")
+                    .bold()
+                Text(user.email)
+            }.padding()
+            HStack{
+                Text("Member Since")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated,time: .shortened))")
+            }.padding()
+        }
+        )
+        
+        //Sign out
+        CustomButton(title: "Logout", background: .blue, action: {
+            //logout action
+            viewModel.logout()
+        })
+        .frame(height: 80)
+        .padding()
+        
+        Spacer()
+        
     }
 }
 
