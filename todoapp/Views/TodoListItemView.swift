4d62e1be818830220b886224cb7835c392603a18
Raj Prajapati <raj.prajapati@waterflow.technology>
2025-03-25 15:35:42 +0545

    chore: todo list item UI

--- todoapp/Views/TodoListItemView.swift
+++ todoapp/Views/TodoListItemView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct TodoListItemView: View {
+    let item: TodoListItem
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack(alignment: .leading){
+                Text(item.title)
+                    .font(.body)
+                    .bold()
+                Text("\(Date.init(timeIntervalSince1970: item.dueDate).formatted(date: .abbreviated,time: .shortened))")
+                    .font(.footnote)
+                    .foregroundColor(Color(.secondaryLabel))
+            }
+            Spacer()
+            Button(
+                action:{
+                    //done action
+                },
+                label: {
+                    Image( systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                })
+        }
     }
 }
 
 #Preview {
-    TodoListItemView()
+    TodoListItemView(item:  TodoListItem(id: "123", title: "asda", dueDate: Date().timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false))
 }
--- todoapp/Views/TodoListView.swift
+++ todoapp/Views/TodoListView.swift
@@ -6,19 +6,39 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct TodoListView: View {
     @StateObject var viewModel = TodoListViewViewModel();
+    @FirestoreQuery var items: [TodoListItem]
     
     private let userId: String;
     
     init(userId: String ) {
         self.userId = userId
+        
+        //our todos are located at below in Firestore.
+        // user/<id>/todos/<entries>
+        //underscore is conevtion for property wrapper -  @FirestoreQuery is a property wrapper.
+        self._items = FirestoreQuery(collectionPath: "user/\(userId)/todos")
+ 
     }
     var body: some View {
         NavigationView{
             VStack{
-                
+                List(items){
+                    item in
+                    TodoListItemView(item: item)
+                        .swipeActions(content: {
+                            Button{
+                                //Delete action
+                                
+                            } label: {
+                                Text("Delete")
+                            }.tint(.red)
+                        })
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("Todo List")
             .toolbar {
@@ -44,5 +64,7 @@ struct TodoListView: View {
 }
 
 #Preview {
-    TodoListView(userId: "UserID")
+    TodoListView(
+        userId: "nL4MVAhHRJh5jDZWTJF2waOHA0C3"
+    )
 }
